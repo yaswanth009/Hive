d96f9dac503b443ae9fc3f4ab61b47bea86e1dcf
Yaswanth A <yash.bigdata@hotmail.com>
2019-04-23 15:52:30 -0400

    Update creation.sh

--- creation.sh
+++ creation.sh
@@ -1,5 +1,5 @@
-#kinit -k -t "yalla1.keytab" "yalla1@HPC.FORD.COM"
-hdfs dfs -copyFromLocal /s/yalla1/FORD_PIN22RGN_FT.txt /project/dz/collab/dopsmdm_test/File/
+
+hdfs dfs -copyFromLocal /s/yaswanth/TextFile.txt /project/dz/test/File/
 
 hive -f table_creation.hql
 
